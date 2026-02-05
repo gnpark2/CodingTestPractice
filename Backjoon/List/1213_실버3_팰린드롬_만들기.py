9daa73244146d30c6ce9f61bd83d03ac5bf8e047
gnpark2 <gnpark2@gmail.com>
2026-02-06 02:47:10 +0900

    백준 1213 실버3 팰린드롬 만들기 해결

---
+++ Backjoon/List/1213_실버3_팰린드롬_만들기.py
@@ -0,0 +1,76 @@
+"""
+백준 1213번: 실버3 팰린드롬 만들기
+
+문제
+임한수와 임문빈은 서로 사랑하는 사이이다.
+
+임한수는 세상에서 팰린드롬인 문자열을 너무 좋아하기 때문에, 둘의 백일을 기념해서 임문빈은 팰린드롬을 선물해주려고 한다.
+
+임문빈은 임한수의 영어 이름으로 팰린드롬을 만들려고 하는데, 임한수의 영어 이름의 알파벳 순서를 적절히 바꿔서 팰린드롬을 만들려고 한다.
+
+임문빈을 도와 임한수의 영어 이름을 팰린드롬으로 바꾸는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 임한수의 영어 이름이 있다. 알파벳 대문자로만 된 최대 50글자이다.
+
+출력
+첫째 줄에 문제의 정답을 출력한다. 만약 불가능할 때는 "I'm Sorry Hansoo"를 출력한다. 정답이 여러 개일 경우에는 사전순으로 앞서는 것을 출력한다.
+
+--------------------------------------------------------------
+
+예제 입력 1 
+AABB
+예제 출력 1 
+ABBA
+
+예제 입력 2 
+AAABB
+예제 출력 2 
+ABABA
+
+예제 입력 3 
+ABACABA
+예제 출력 3 
+AABCBAA
+
+예제 입력 4 
+ABCD
+예제 출력 4 
+I'm Sorry Hansoo
+
+"""
+
+import sys
+input = sys.stdin.readline
+
+name = input().strip()
+name = sorted(name)
+
+count_dict = dict()
+
+for char in name:
+    if char in count_dict:
+        count_dict[char] += 1
+    else:
+        count_dict[char] = 1
+
+odd_count = 0
+odd_char = ''
+
+for key, value in count_dict.items():
+    if value % 2 == 1:
+        odd_count += 1
+        odd_char = key
+if odd_count > 1:
+    print("I'm Sorry Hansoo")
+    sys.exit()
+
+first_half = ''
+
+for key in sorted(count_dict.keys()):
+    first_half += key * (count_dict[key] // 2)
+second_half = first_half[::-1]
+if odd_count == 1:
+    print(first_half + odd_char * count_dict[odd_char] + second_half)
+else:
+    print(first_half + second_half)
